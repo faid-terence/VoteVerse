52ace63ee0c2c83ebfe461553911ed97ff00ad2e
Terence Faid JABO <faidterence@outlook.com>
2024-08-29 21:47:54 +0200

    feat: configure database connection and implement Spring Security
                   - Set up database configurations (DataSource, JPA properties)
                   - Implement Spring Security for user authentication and authorization
                   - Configure BCrypt password encoding
                   - Define roles and permissions for ADMIN and VOTER
                   - Secure endpoints with role-based access control

---
+++ src/main/java/com/faidterence/VoteVerse/VoteVerseApplication.java
@@ -0,0 +1,13 @@
+package com.faidterence.VoteVerse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoteVerseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoteVerseApplication.class, args);
+	}
+
+}
